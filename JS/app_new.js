dd46973750ce00e52568ae7d99666952cafe7efa
cristian <cristianpglez0@gmail.com>
2023-12-20 18:15:55 +0100

    update security initial page

--- JS/app_new.js
+++ JS/app_new.js
@@ -8,21 +8,22 @@
 const nickinput=document.getElementById("nick");
 const tamanoinput=document.getElementById("tamano");
 const formEntrada=document.getElementById("formEntrada");
+const error=document.getElementById("error");
 
 
 //Funciones de evento
 function comprobarForm(event){
     //comprobar cambios
-    if(nickinput.value.length==0)
+    if(nickinput.value.match(/(?<!\S)[0-9]/))
     {
-        console.log("no hay nick");
         nickinput.focus();
         event.preventDefault();
+        error.innerText="El campo de nick no puede comenzar con un numero"
         return false;
     }else if(tamanoinput.value=="0"){
-        console.log("no se ha seleccionado tamaño de panel");
         tamanoinput.focus();
         event.preventDefault();
+        error.innerText="debe seleccionar un tamaño de panel"
         return false;
     }
     return true;
